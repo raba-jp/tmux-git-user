8c30e8148bfc5e374a409af21d8a23cbe9a57553
Hiroki Sakuraba <sakuraba@tech.gr.jp>
2017-04-27 17:06:47 +0900

    Fix bug

--- scripts/git_user.sh
+++ scripts/git_user.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 git_dir="$(cd `tmux display-message -p -F "#{pane_current_path}"`)"
-git_user="$(git config --get user.name)"
-git_email="$(git config --get user.email)"
+git_user="$(git config --get --local user.name)"
+git_email="$(git config --get --local user.email)"
 
-if [ ! -z ${git_user} ]; then
+if [ ! -z ${git_email} ]; then
   printf %s "[${git_user}#${git_email}]"
 fi
 
